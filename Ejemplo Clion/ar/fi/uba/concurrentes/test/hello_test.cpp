e2844da72671657bde86e2ebdddd836cbdb100a5
JuanDausa <juandausa@gmail.com>
2016-03-13 15:35:12 -0300

    Se agrega un ejemplo de clion con la biblioteca de test incluída.

---
+++ Ejemplo Clion/ar/fi/uba/concurrentes/test/hello_test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by root on 9/7/15.
+//
+
+#include "hello_test.h"
+
+TEST(MathTest, TwoPlusTwoEqualsFour) {
+    EXPECT_EQ(2 + 2, 4);
+}
+
---
+++ Ejemplo Clion/ar/fi/uba/concurrentes/test/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by lucas on 13/09/15.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::FLAGS_gtest_output = "xml:./res.xml";
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
