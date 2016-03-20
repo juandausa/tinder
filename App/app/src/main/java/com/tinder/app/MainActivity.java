3399f63a479138d2c39862a65d5f239d4549e6c5
GFibrizo <zebas.graffe@gmail.com>
2016-03-19 23:34:24 -0300

    Agregado menu deslizante vacio, colores custom y estilos custom

--- App/app/src/main/java/com/tinder/app/MainActivity.java
+++ App/app/src/main/java/com/tinder/app/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.mikepenz.materialdrawer.DrawerBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new DrawerBuilder().withActivity(this).build();
     }
 
     @Override
