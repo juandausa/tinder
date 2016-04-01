3499d49de20cbd8a078d6ddae2cca2252b2272c4
JuanDausa <juandausa@gmail.com>
2016-03-31 23:21:25 -0300

    Issue #28: Se agregan nuevos test a modo de documentación.

--- AppServer/ar/fi/uba/tallerii/test/rocksdb_test.cpp
+++ AppServer/ar/fi/uba/tallerii/test/rocksdb_test.cpp
@@ -30,3 +30,27 @@ TEST(RocksDB, ReadAndWriteString) {
     EXPECT_EQ(value.compare(retrivedValue), 0);
 }
 
+TEST(RocksDB, UpdateString) {
+    rocksdb::DB *database = create_database();
+    if (database == NULL) {
+        return;
+    }
+    std::string value = "ValorUno", key = "ClaveUno", retrivedValue;
+    rocksdb::Status put_opreation_result = database->Put(rocksdb::WriteOptions(), key, value);
+    value = "ValorDos";
+    put_opreation_result =  database->Put(rocksdb::WriteOptions(), key, value);
+    rocksdb::Status read_operation_result = database->Get(rocksdb::ReadOptions(), key, &retrivedValue);
+    EXPECT_EQ(value.compare(retrivedValue), 0);
+}
+
+TEST(RocksDB, DeleteString) {
+    rocksdb::DB *database = create_database();
+    if (database == NULL) {
+        return;
+    }
+    std::string value = "ValorUno", key = "ClaveUno", retrivedValue;
+    rocksdb::Status put_opreation_result = database->Put(rocksdb::WriteOptions(), key, value);
+    rocksdb::Status delete_opreation_result =  database->Delete(rocksdb::WriteOptions(), key);
+    rocksdb::Status read_operation_result = database->Get(rocksdb::ReadOptions(), key, &retrivedValue);
+    EXPECT_TRUE(!read_operation_result.ok());
+}
