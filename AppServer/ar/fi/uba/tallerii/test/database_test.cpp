9f77e4055679cce3f98a0ba270581209cf003bb5
Juan Dausa <juandausa@gmail.com>
2016-04-05 20:04:55 -0300

    Issue #28: Se agregan tests sobre el wrapper de la base de datos.

---
+++ AppServer/ar/fi/uba/tallerii/test/database_test.cpp
@@ -0,0 +1,34 @@
+//
+// Created by juan on 05/04/16.
+//
+
+#include "include/database_test.h"
+#include "DataBase.h"
+
+TEST(DataBaseWrapper, ReadAndWriteString) {
+    DataBase db("/tmp/testdb10");
+    std::string value = "Valor", key = "Clave", retrivedValue;
+    db.Set(key, value);
+    db.Get(key, &retrivedValue);
+    EXPECT_EQ(value.compare(retrivedValue), 0);
+}
+
+TEST(DataBaseWrapper, UpdateString) {
+    DataBase db("/tmp/testdb2");
+    std::string value = "ValorUno", key = "ClaveUno", retrivedValue;
+    db.Set(key, value);
+    db.Get(key, &retrivedValue);
+    EXPECT_EQ(value.compare(retrivedValue), 0);
+    value = "ValorDos";
+    db.Set(key, value);
+    db.Get(key, &retrivedValue);
+    EXPECT_EQ(value.compare(retrivedValue), 0);
+}
+
+TEST(DataBaseWrapper, DeleteString) {
+    DataBase db("/tmp/testdb3");
+    std::string value = "ValorUno", key = "ClaveUno", retrivedValue;
+    db.Set(key, value);
+    EXPECT_TRUE(db.Delete(key));
+    EXPECT_FALSE(db.Get(key, &retrivedValue));
+}
---
+++ AppServer/ar/fi/uba/tallerii/test/include/database_test.h
@@ -0,0 +1,10 @@
+//
+// Created by juan on 05/04/16.
+//
+
+#ifndef TINDER_DATABASE_TEST_H
+#define TINDER_DATABASE_TEST_H
+
+#include <gtest/gtest.h>
+
+#endif //TINDER_DATABASE_TEST_H
