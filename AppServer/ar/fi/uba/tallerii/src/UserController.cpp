83754e27587e5e8912735692578ca4ffc3990167
Juan Dausa <juandausa@gmail.com>
2016-04-06 23:04:35 -0300

    Issue #16: Se agregan mensajes de log.

--- AppServer/ar/fi/uba/tallerii/src/UserController.cpp
+++ AppServer/ar/fi/uba/tallerii/src/UserController.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "UserController.h"
-
+#include <glog/logging.h>
 UserController :: UserController() {}
 
 void UserController :: handle_login(struct mg_connection *nc, struct http_message *hm) {
@@ -11,11 +11,12 @@ void UserController :: handle_login(struct mg_connection *nc, struct http_messag
 
     /* Get form variables */
     mg_get_http_var(&hm->query_string, "userId", user_id, sizeof(user_id));
-
+    LOG(INFO) << "Proccesing login for user: " << user_id << ".";
     /* Send headers */
     mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
 
     /* Compute the result and send it back as a JSON object */
     mg_printf_http_chunk(nc, "{ \"user\": { \"userId\" : \"%s\" } }", user_id, password);
     mg_send_http_chunk(nc, "", 0);  /* Send empty chunk, the end of response */
+    LOG(INFO) << "Login succeeded for user: " << user_id << ".";
 }
