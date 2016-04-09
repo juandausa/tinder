7883b18765b7819d454d02b7012c9e0e4a50ad0f
GFibrizo <zebas.graffe@gmail.com>
2016-04-09 15:04:42 -0300

    Agregada pantalla dummy de ajustes

--- App/app/src/main/java/com/tinder_app/SettingsActivity.java
+++ App/app/src/main/java/com/tinder_app/SettingsActivity.java
@@ -1,11 +1,19 @@
 package com.tinder_app;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.DragEvent;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.edmodo.rangebar.RangeBar;
 
 /**
  * This Activity holds the settings controls that allows the user customize some features.
@@ -18,15 +26,122 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        discoverDistanceBarSetUp();
+        ageBarSetUp();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.settings_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+
+    private void discoverDistanceBarSetUp() {
+        SeekBar seekBar = (SeekBar) findViewById(R.id.distance);
+        seekBar.setMax(100);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView rangeDistance = (TextView) findViewById(R.id.distance_label);
+                rangeDistance.setText(Integer.toString(progress) + " km");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+
+    private void ageBarSetUp() {
+        // https://github.com/edmodo/range-bar/wiki
+        // Documentation of RangeBar
+        RangeBar rangebar = (RangeBar) findViewById(R.id.age_rangebar);
+        rangebar.setTickCount(100);
+        rangebar.setTickHeight(25);
+        rangebar.setBarWeight(6);
+        rangebar.setBarColor(android.R.color.holo_red_light);
+        rangebar.setOnRangeBarChangeListener(new RangeBar.OnRangeBarChangeListener() {
+            @Override
+            public void onIndexChangeListener(RangeBar rangeBar, int leftThumbIndex, int rightThumbIndex) {
+                //Code using the leftThumbIndex and rightThumbIndex to update the index values.
+                TextView distance = (TextView) findViewById(R.id.age_label);
+                String ageRangeLabel = Integer.toString(leftThumbIndex) + " - "
+                        + Integer.toString(rightThumbIndex);
+                distance.setText(ageRangeLabel);
             }
         });
+    }
+
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+
+    private void menSwitchSetUp() {
+        Switch men = (Switch) findViewById(R.id.men);
+        Switch women = (Switch) findViewById(R.id.women);
+        GenderObject menObject = new GenderObject("Hombres", men);
+        GenderObject womenObject = new GenderObject("mujeres", women);
+        genderSwitchesSetUp(menObject, womenObject);
+        genderSwitchesSetUp(womenObject, menObject);
+    }
+
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+
+    private void womenSwitchSetUp() {
+
+    }
+
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+
+    private void genderSwitchesSetUp(final GenderObject gender, final GenderObject oppositeGender) {
+
+        gender.getSwitch().setTextOn("");
+        gender.getSwitch().setTextOff("");
+        gender.getSwitch().setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                TextView genderLabel = (TextView) findViewById(R.id.gender_label);
+                if (gender.getSwitch().isChecked()) {
+                    if (oppositeGender.getSwitch().isChecked()) {
+                        genderLabel.setText("Hombres y mujeres");
+
+                    } else {
+                        genderLabel.setText(gender.getLabel());
+                    }
+                }
+                else if (oppositeGender.getSwitch().isChecked()) {
+                    genderLabel.setText(oppositeGender.getLabel());
+
+                }
+                genderLabel.invalidate();
+                return true;
+            }
+        });
+    }
+
+    /**********************************************************************************************/
+    /*********************                  AUXILIAR CLASS              ***************************/
+    /**********************************************************************************************/
+
+    private class GenderObject {
+
+        private String genderLabel;
+        private Switch genderSwitch;
+
+        public GenderObject(String genderLabel, Switch genderSwitch) {
+            this.genderLabel = genderLabel;
+            this.genderSwitch = genderSwitch;
+        }
+
+        public String getLabel() {
+            return this.genderLabel;
+        }
+
+        public Switch getSwitch() {
+            return this.genderSwitch;
+        }
     }
 
 }
