17fb449bec24553ef06d108d0fb4e0d3e98c1683
Juan Dausa <juandausa@gmail.com>
2016-04-26 20:28:05 -0300

    Issue #35: Se agregan constantes para las urls del shared server.

--- AppServer/ar/fi/uba/tallerii/src/Constant.cpp
+++ AppServer/ar/fi/uba/tallerii/src/Constant.cpp
@@ -10,3 +10,5 @@ std::string Constant :: database_path = "/tmp/tinderdb";
 std::string Constant :: security_token_prefix = "token-";
 std::size_t Constant :: random_characters_quantity = 5;
 std::string Constant :: filters_prefix = "preferences-";
+std::string Constant :: update_user_info_url = "https://enigmatic-scrubland-75073.herokuapp.com/users/";
+std::string Constant :: get_user_info_url = "https://enigmatic-scrubland-75073.herokuapp.com/users/";
