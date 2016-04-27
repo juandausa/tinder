b7d95256d4faae4721cc227650bda3a20a407158
jasmina.sella <jasmina.sella@fdvsolutions.com>
2016-04-27 13:32:39 -0300

    Issue #31: armado de Json para envio de request al SharedServer

--- AppServer/ar/fi/uba/tallerii/src/UserController.cpp
+++ AppServer/ar/fi/uba/tallerii/src/UserController.cpp
@@ -4,18 +4,21 @@
 
 #include "UserController.h"
 #include "CurlWrapper.h"
+#include <jsoncpp/json/json.h>
 #include "Response.h"
 #include "DataBase.h"
 #include <glog/logging.h>
 #include <string>
-#include <jsoncpp/json/json.h>
+
 
 
 UserController :: UserController(UserService user_service) : user_service(user_service) {
 }
 
-/* Private methods declarion */
+/* Private methods declaration */
 std::string make_body_for_login_response(const std::string user_id, const std::string token);
+Json::Value create_json_array(Json::Value array);
+
 /* End private methods */
 
 void UserController :: handle_login(struct mg_connection *nc, struct http_message *hm, Response response) {
@@ -36,9 +39,87 @@ void UserController :: handle_login(struct mg_connection *nc, struct http_messag
 }
 
 void UserController :: handle_registration(struct mg_connection *nc, struct http_message *hm, Response response) {
-    char user_id[255];
-    mg_get_http_var(&hm->body, "userId", user_id, sizeof(user_id));
-    // CREO que si agarro directo el body ya está
+    Json::Value root;
+    Json::Reader reader;
+
+    bool parsingSuccessful = reader.parse(hm->body.p, root, true);
+    if (!parsingSuccessful) {
+        // report to the user the failure and their locations in the document.
+        std::cout  << "Failed to parse configuration\n";
+//        return;
+    }
+
+    std::string user_id = root.get("user_id", "").asString();
+    std::string name = root.get("name", "").asString();
+    std::string birthday = root.get("birthday", "").asString();
+    std::string alias = root.get("alias", "").asString();
+    std::string email = root.get("email", "").asString();
+    std::string photo_profile = root.get("photo_profile", "").asString();
+    Json::Value music = root["interests"]["music"];
+    Json::Value movies = root["interests"]["movies"];
+    Json::Value likes = root["interests"]["likes"];
+    Json::Value television = root["interests"]["television"];
+    Json::Value games = root["interests"]["games"];
+    Json::Value books = root["interests"]["books"];
+
+    double latitude = root["location"].get("latitude", 0).asDouble();
+    double longitude = root["location"].get("longitude", 0).asDouble();
+
+
+    std::cout << music << std::endl;
+    std::cout << music.size() << std::endl;
+    std::cout << music[0] << std::endl;
+    std::cout << music[1] << std::endl;
+
+    Json::Value event;
+    Json::Value music_array(Json::arrayValue);
+    Json::Value movies_array(Json::arrayValue);
+    Json::Value likes_array(Json::arrayValue);
+    Json::Value television_array(Json::arrayValue);
+    Json::Value games_array(Json::arrayValue);
+    Json::Value books_array(Json::arrayValue);
+
+    music_array.append(Json::Value("radiohead"));
+    music_array.append(Json::Value("eminem"));
+    movies_array.append(Json::Value("Robocop"));
+    likes_array.append(Json::Value("Robots"));
+    television_array.append(Json::Value("the simpsons"));
+    games_array.append(Json::Value("Counter Strike"));
+    books_array.append(Json::Value("Steve Jobs"));
+
+    event["user_id"] = user_id;
+    event["name"] = name;
+    event["birthday"] = birthday;
+    event["alias"] = alias;
+    event["email"] = email;
+    event["photo_profile"] = photo_profile;
+    event["interest"]["music"] = music_array;
+    event["interest"]["movies"] = movies_array;
+    event["interest"]["likes"] = likes_array;
+    event["interest"]["television"] = television_array;
+    event["interest"]["games"] = games_array;
+    event["interest"]["books"] = books_array;
+    event["location"]["latitude"] = latitude;
+    event["location"]["longitude"] = longitude;
+    std::cout << event << std::endl;
+
+//    event["user_id"] = user_id;
+//    event["name"] = name;
+//    event["birthday"] = birthday;
+//    event["alias"] = alias;
+//    event["email"] = email;
+//    event["photo_profile"] = photo_profile;
+//    event["interest"]["music"] = create_json_array(music);
+//    event["interest"]["movies"] = create_json_array(movies);
+//    event["interest"]["likes"] = create_json_array(likes);
+//    event["interest"]["television"] = create_json_array(television);
+//    event["interest"]["games"] = create_json_array(games);
+//    event["interest"]["books"] = create_json_array(books);
+//    event["location"]["latitude"] = latitude;
+//    event["location"]["longitude"] = longitude;
+//    std::cout << event << std::endl;
+
+
     CurlWrapper curlWrapper = CurlWrapper();
     std::string url = "https://enigmatic-scrubland-75073.herokuapp.com/users";
     curlWrapper.set_post_url(url);
@@ -48,6 +129,7 @@ void UserController :: handle_registration(struct mg_connection *nc, struct http
     LOG(INFO) << "Proccesing registration for user: ";
     if (res) {
         response.SetCode(200);
+        response.SetBody(user_id);
         response.Send();
         LOG(INFO) << "Login succeeded";
     } else {
@@ -55,22 +137,6 @@ void UserController :: handle_registration(struct mg_connection *nc, struct http
         response.Send();
         LOG(INFO) << "Login failed";
     }
-//    mg_get_http_var(&hm->body, "userId", user_id, sizeof(user_id));
-//    mg_get_http_var(&hm->body, "name", name, sizeof(name));
-//    mg_get_http_var(&hm->body, "birthday", birthday, sizeof(birthday));
-//    mg_get_http_var(&hm->body, "alias", alias, sizeof(alias));
-//    mg_get_http_var(&hm->body, "email", email, sizeof(email));
-//    mg_get_http_var(&hm->body, "photo_profile", photo_profile, sizeof(photo_profile));
-
-//    if (this->user_service.register_user(user_id, name, birthday, alias, email, photo_profile)) {
-//        response.SetCode(200);
-//        response.Send();
-//        LOG(INFO) << "Registration succeeded for user: " << user_id << ".";
-//    } else {
-//        response.SetCode(500);
-//        response.Send();
-//        LOG(INFO) << "Registration failed for user: " << user_id << ".";
-//    }
 }
 
 std::string make_body_for_login_response(const std::string user_id, const std::string token) {
@@ -78,4 +144,13 @@ std::string make_body_for_login_response(const std::string user_id, const std::s
     body.append(user_id).append("\" , \"token\" : \"");
     body.append(token).append("\" } }");
     return body;
+}
+
+Json::Value create_json_array(Json::Value array) {
+    Json::Value json_array(Json::arrayValue);
+    for (unsigned int i = 0; i < array.size(); i++) {
+        json_array.append(Json::Value(array[i]));
+    }
+    std::cout << json_array << std::endl;
+    return json_array;
 }
