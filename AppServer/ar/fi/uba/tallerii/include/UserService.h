7d3807bc06d3d26b2410401dd71306c1c0bfa27f
jasmina.sella <jasmina.sella@fdvsolutions.com>
2016-05-08 19:29:36 -0300

    Issue #13: cambio firma de registro de usuario.

--- AppServer/ar/fi/uba/tallerii/include/UserService.h
+++ AppServer/ar/fi/uba/tallerii/include/UserService.h
@@ -19,9 +19,7 @@ public:
     UserService(DataBase & db);
     ~UserService();
     bool isUserRegistered(const std::string userId);
-    bool registerUser(const std::string userId, const std::string name,
-                       const std::string birthday, const std::string alias,
-                       const std::string email, const std::string photoProfile);
+    bool registerUser(const std::string userId, const std::string token);
     std::string getSecurityToken(const std::string userName);
     bool isTokenValid(const std::string userName, const std::string token);
     std::string getExternalUserId(std::string userId);
