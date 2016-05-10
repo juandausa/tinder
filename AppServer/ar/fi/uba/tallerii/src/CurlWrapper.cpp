bd891f7381f844fbfcab46587606d6367aea301b
Juan Dausa <juandausa@gmail.com>
2016-05-10 19:21:49 -0300

    Issue #35: Se agregan los headers al método put del ccurlwrapper.

--- AppServer/ar/fi/uba/tallerii/src/CurlWrapper.cpp
+++ AppServer/ar/fi/uba/tallerii/src/CurlWrapper.cpp
@@ -49,6 +49,7 @@ void CurlWrapper::set_put_data(const std::string data, std::string &readBuffer) 
     if (curl) {
         struct curl_slist *headers = NULL;
         headers = curl_slist_append(headers, "Accept: application/json");
+        headers = curl_slist_append(headers, "Content-Type: application/json");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
