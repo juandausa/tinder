dee380e1aee4b150b32863467f9d32c3ce02c842
GFibrizo <zebas.graffe@gmail.com>
2016-05-24 22:13:24 -0300

    Cambiado directorio de almacenado de imagenes en EditProfileActivity. Issue #11.

--- App/app/src/main/java/com/tinder_app/EditProfileActivity.java
+++ App/app/src/main/java/com/tinder_app/EditProfileActivity.java
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
@@ -371,9 +370,9 @@ public class EditProfileActivity extends AppCompatActivity {
      */
     private String saveProfilePhoto() {
         verifyStoragePermissions(this);
+        //Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
         try {
-            String filePath = Environment.
-                    getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            String filePath = getCacheDir()
                     + "/" + Constants.PHOTO_PROFILE + ".jpg";
 
             /*String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()
