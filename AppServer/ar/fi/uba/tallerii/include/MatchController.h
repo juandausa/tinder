554f4674824523ab08f403c6693506a8034d390a
Juan Dausa <juandausa@gmail.com>
2016-05-26 22:26:05 -0300

    Issue #38: Se agrega al controlador de matches el UserService en la construcción.

--- AppServer/ar/fi/uba/tallerii/include/MatchController.h
+++ AppServer/ar/fi/uba/tallerii/include/MatchController.h
@@ -5,15 +5,17 @@
 #ifndef TINDER_MATCHCONTROLLER_H
 #define TINDER_MATCHCONTROLLER_H
 #include "MatchService.h"
+#include "UserService.h"
 #include "RequestParser.h"
 #include "Response.h"
 
 class MatchController {
 public:
-    MatchController(MatchService match_service);
+    MatchController(MatchService match_service, UserService user_service);
     virtual void handle_get_candidates(RequestParser requestParser, Response response);
 private:
     MatchService match_service;
+    UserService user_service;
 };
 
 
--- AppServer/ar/fi/uba/tallerii/src/MatchController.cpp
+++ AppServer/ar/fi/uba/tallerii/src/MatchController.cpp
@@ -4,7 +4,7 @@
 
 #include "MatchController.h"
 
-MatchController ::MatchController(MatchService match_service) : match_service(match_service) {
+MatchController ::MatchController(MatchService match_service, UserService user_service) : match_service(match_service), user_service(user_service) {
 }
 
 void MatchController :: handle_get_candidates(RequestParser requestParser, Response response) {
