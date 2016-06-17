b042b45919c6a558d49b2e32b9674caa69fdb2ec
Juan Dausa <juandausa@gmail.com>
2016-06-17 18:21:33 -0300

    Issue #58: Se agrega el controlador de mensajes.

--- AppServer/ar/fi/uba/tallerii/include/MessagesController.h
+++ AppServer/ar/fi/uba/tallerii/include/MessagesController.h
@@ -4,17 +4,27 @@
 
 #ifndef TINDER_MESSAGESCONTROLLER_H
 #define TINDER_MESSAGESCONTROLLER_H
+
 #include "MessagesService.h"
 #include "UserService.h"
 #include "RequestParser.h"
 #include "Response.h"
+#include "Message.h"
+#include <jsoncpp/json/json.h>
+#include <string>
 
 class MessagesController {
 public:
     MessagesController(MessagesService messages_service, UserService user_service);
+    virtual void handleAddMessage(RequestParser requestParser, Response response);
+    virtual void handleGetMessages(RequestParser requestParser, Response response);
+
 private:
     MessagesService messagesService;
     UserService userService;
+    Json::Reader reader;
+    Json::FastWriter fastWriter;
+    Message getMessageFromRequest(RequestParser request);
 };
 
 
--- AppServer/ar/fi/uba/tallerii/src/MessagesController.cpp
+++ AppServer/ar/fi/uba/tallerii/src/MessagesController.cpp
@@ -8,3 +8,52 @@
 MessagesController::MessagesController(MessagesService messages_service, UserService user_service) :
         messagesService(messages_service), userService(user_service) {
 }
+
+Message MessagesController::getMessageFromRequest(RequestParser request) {
+    Json::Value message;
+    bool parsingSuccessful = this->reader.parse(request.getBody(), message, true);
+    if (!parsingSuccessful) {
+        Message emptyMessage;
+        return emptyMessage;
+    }
+
+    Message result(request.getResourceId(), message.get("to_user_id", "").asString(), message.get("message", "").asString());
+    return result;
+}
+
+void MessagesController::handleAddMessage(RequestParser requestParser, Response response) {
+    LOG(INFO) << "Adding message from user: '" << requestParser.getResourceId() << "'";
+    Message message = this->getMessageFromRequest(requestParser);
+    std::string sender = message.getSender();
+    std::string reciever = message.getReciever();
+    if ((sender.length() == 0) || (reciever.length() == 0)) {
+        response.SetCode(500);
+        response.SetBody("Bad Request, no sender or reciever detected.");
+        LOG(WARNING) << "Bad Request, no sender or reciever detected. Sender: '" << sender << "' or user: '" <<
+        reciever << "'";
+    } else if ((!userService.isUserRegistered(sender)) || (!userService.isUserRegistered(reciever))) {
+        response.SetCode(500);
+        response.SetBody(
+                "Bad Request, sender '" + sender + "' or reciever '" + reciever + " is not registered.");
+        LOG(WARNING) << "Error for addLike. User: '" << sender << "' or user: '" << reciever <<
+        "' is not registered";
+    } else {
+        if (this->messagesService.addMessage(message)) {
+            response.SetCode(200);
+            LOG(INFO) << "Message from user: '" << sender << "' to user: '" << reciever << "' added";
+        } else {
+            response.SetCode(500);
+            response.SetBody("Error for addMessage, error on save.");
+            LOG(WARNING) << "Error for addMessage, error on save. From user: '" << sender << "' to user: '" <<
+            reciever << "'";
+        }
+    }
+
+    response.Send();
+}
+
+void MessagesController::handleGetMessages(RequestParser requestParser, Response response) {
+    response.SetCode(200);
+    response.SetBody("RECIBIDO");
+    response.Send();
+}
