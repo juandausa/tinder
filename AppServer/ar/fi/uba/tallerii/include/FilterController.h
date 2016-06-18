972db1419babda55ae858c844bee03ee7f0689c1
Juan Dausa <juandausa@gmail.com>
2016-06-18 17:56:31 -0300

    Issue #44: Se cambian las respuestas de la operaciones de preferencias.

--- AppServer/ar/fi/uba/tallerii/include/FilterController.h
+++ AppServer/ar/fi/uba/tallerii/include/FilterController.h
@@ -9,6 +9,7 @@
 #include "FilterService.h"
 #include "Response.h"
 #include "RequestParser.h"
+#include <jsoncpp/json/json.h>
 
 class FilterController {
 public:
@@ -17,6 +18,12 @@ public:
     virtual void handle_get_filters(RequestParser requestParser, Response response);
 private:
     FilterService filter_service;
+    Json::Reader reader;
+    Json::FastWriter fastWriter;
+    std::string makeResponseBodyForGet(const std::string info);
+    std::string getErrorResponseBody();
+    std::string getSucceedResponseBody();
+
 };
 
 #endif //TINDER_FILTERCONTROLLER_H
