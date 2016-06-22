89f3b63061af2ead7282e47a3a590ee18570c08e
Juan Dausa <juandausa@gmail.com>
2016-06-22 00:14:17 -0300

    Issue #78: Se agregan tests sobre la clase Request.

---
+++ AppServer/source/test/include/request_test.h
@@ -0,0 +1,13 @@
+//
+// Copyright 2016 FiUBA
+//
+
+#ifndef TINDER_REQUEST_TEST_H
+#define TINDER_REQUEST_TEST_H
+
+#include "Response.h"
+#include "Request.h"
+#include <Constant.h>
+#include <gtest/gtest.h>
+
+#endif //TINDER_REQUEST_TEST_H
