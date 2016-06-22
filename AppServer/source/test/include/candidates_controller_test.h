9d5f8ed428c099481d00f28ed83f7c63f89dfc4e
Juan Dausa <juandausa@gmail.com>
2016-06-22 00:02:03 -0300

    Issue #78: Se agrega un nuevo test sobre el GetCandidatesController.

---
+++ AppServer/source/test/include/candidates_controller_test.h
@@ -0,0 +1,13 @@
+//
+// Created by juan on 08/04/16.
+//
+
+#ifndef TINDER_CANDIDATES_CONTROLLER_TEST_H
+#define TINDER_CANDIDATES_CONTROLLER_TEST_H
+
+#include "GetCandidatesController.h"
+#include "DataBase.h"
+#include <gtest/gtest.h>
+
+
+#endif //TINDER_CANDIDATES_CONTROLLER_TEST_H
