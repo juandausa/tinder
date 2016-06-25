4e52345c5859139d467a33a9fdc2bd32b65722ff
Juan Dausa <juandausa@gmail.com>
2016-06-25 15:50:04 -0300

    Issue #83: Se tiene en cuenta más firmas para los tests del log wrapper.

--- AppServer/source/test/log_wrapper_test.cpp
+++ AppServer/source/test/log_wrapper_test.cpp
@@ -4,14 +4,29 @@
 
 #include "log_wrapper_test.h"
 
-TEST(LogWrapper, Write) {
+TEST(LogWrapper, Print) {
     Log *logger = Log::getInstance();
+    std::string message("hello 2");
     logger->printLog("hello", Log::WARNING);
     logger->printLog("hello", Log::INFO);
+    logger->printLog(message, Log::WARNING);
+    logger->printLog(message, Log::INFO);
+}
+
+TEST(LogWrapper, Write) {
+    Log *logger = Log::getInstance();
+    std::string message("hello 2");
+    logger->writeLog("hello", Log::WARNING);
+    logger->writeLog("hello", Log::INFO);
+    logger->writeLog(message, Log::WARNING);
+    logger->writeLog(message, Log::INFO);
 }
 
 TEST(LogWrapper, WriteAndPrint) {
+    std::string message("hello 2");
     Log *logger = Log::getInstance();
     logger->writeAndPrintLog("hello", Log::WARNING);
     logger->writeAndPrintLog("hello", Log::INFO);
+    logger->writeAndPrintLog(message, Log::WARNING);
+    logger->writeAndPrintLog(message, Log::INFO);
 }
